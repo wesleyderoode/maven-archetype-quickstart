c87a3c657ab30ea3d2131ab3184bcf02e542f5dd
Wesley de Roode <w.de.roode@live.nl>
2019-01-17 14:52:37 +0100

    (inital) empty project

---
+++ src/main/java/nl/wderoode/App.java
@@ -0,0 +1,7 @@
+package nl.wderoode;
+
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/nl/wderoode/AppTest.java
@@ -0,0 +1,20 @@
+package nl.wderoode;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertThat( true ).isTrue();
+    }
+}
